b6d3f5643bcaf2d3185bbf62e4c5c47c47d2a496
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-12-13 20:06:44 +0100

    Refactoring

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import render_template
 from flask import request
 from math import factorial
 
+
+
 #Declaring the app
 app = Flask(__name__)
 
@@ -15,33 +17,25 @@ def main():
 
 #Form Submission Route
 @app.route('/send', methods=['POST'])
-
-
 def send(sum=sum):
-    if request.method == 'POST':
-        #Start pulling data from form input
-        n = request.form['n']
-        k = request.form['k']
-        operation = request.form['operation']
-        #Calculation
-        if operation == 'combinations-with-repetitions':
-            try:
-                numerator = factorial(int(n) + int(k) - 1)
-                denominator = factorial(int(k)) * factorial(int(n) - 1)
-                sum = int(numerator) / int(denominator)
+        if request.method == 'POST' and request.form['action'] == "Calculate":
+            #Start pulling data from form input
+                n = request.form['n']
+                k = request.form['k']
+                operation = request.form['operation']
+
+                if operation == 'combinations-with-repetitions':
+                    numerator = factorial(int(n) + int(k) - 1)
+                    denominator = factorial(int(k)) * factorial(int(n) - 1)
+                    sum = int(numerator) / int(denominator)
+
+
+                elif operation == 'combinations-without-repetitions':
+                    numerator = factorial(int(n))
+                    denominator = factorial(int(k)) * factorial(int(n) - int(k))
+                    sum = int(numerator) / int(denominator)
+
                 return render_template('app.html', sum=int(sum))
-            except:
-                return render_template('app.html', sum="An error has been occurred")
-        elif operation == 'combinations-without-repetitions':
-            try:
-                numerator = factorial(int(n))
-                denominator = factorial(int(k)) * factorial(int(n) - int(k))
-                sum = int(numerator) / int(denominator)
-                return render_template('app.html', sum=int(sum))
-            except:
-                return render_template('app.html', sum="An error has been occurred")
-        else:
-            return render_template('app.html')
 
 
 
