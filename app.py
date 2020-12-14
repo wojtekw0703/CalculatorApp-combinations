6ba3dfb7fcbfba1fe4c1feae40f9e6faed0f6baf
Wojciech Wydmuch <wojciechwydmuch73@gmail.com>
2020-12-14 23:04:56 +0100

    Added calculation of combinations without repetition

--- app.py
+++ app.py
@@ -2,9 +2,10 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from math import factorial
+import itertools as it
 
 
-
+my_list=[]
 #Declaring the app
 app = Flask(__name__)
 
@@ -18,24 +19,33 @@ def main():
 #Form Submission Route
 @app.route('/send', methods=['POST'])
 def send(sum=sum):
-        if request.method == 'POST' and request.form['action'] == "Calculate":
+    if request.method == 'POST' and request.form['action'] == "Calculate":
+        try:
             #Start pulling data from form input
-                n = request.form['n']
-                k = request.form['k']
-                operation = request.form['operation']
+            n = request.form['n']
+            k = request.form['k']
+            operation = request.form['operation']
 
-                if operation == 'combinations-with-repetitions':
-                    numerator = factorial(int(n) + int(k) - 1)
-                    denominator = factorial(int(k)) * factorial(int(n) - 1)
-                    sum = int(numerator) / int(denominator)
+            if operation == 'combinations-with-repetitions':
+                numerator = factorial(int(n) + int(k) - 1)
+                denominator = factorial(int(k)) * factorial(int(n) - 1)
+                sum = int(numerator) / int(denominator)
+                for i in range(1, int(n)+1):
+                    my_list.append(i)
+                result = set(list(it.combinations(my_list, int(k))))
 
 
-                elif operation == 'combinations-without-repetitions':
-                    numerator = factorial(int(n))
-                    denominator = factorial(int(k)) * factorial(int(n) - int(k))
-                    sum = int(numerator) / int(denominator)
+            elif operation == 'combinations-without-repetitions':
+                numerator = factorial(int(n))
+                denominator = factorial(int(k)) * factorial(int(n) - int(k))
+                sum = int(numerator) / int(denominator)
+                for i in range(1, int(n)+1):
+                    my_list.append(i)
+                result = set(list(it.combinations(my_list, int(k))))
 
-                return render_template('app.html', sum=int(sum))
+            return render_template('app.html', sum=int(sum), combinations=result)
+        except Exception as err:
+            return render_template('app.html', sum=err)
 
 
 
